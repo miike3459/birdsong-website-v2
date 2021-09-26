4f38debae1d8ed77fdd62b10a97b31dd88ce1ccd
miike3459 <miike3459@users.noreply.github.com>
2021-09-26 00:38:06 +0000

    deploy: 414f3a18137863f08a46b7e81f76d50331e1c053

--- _next/static/l6-BlGKv0gcpLWLdRVhgr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-53ddd840b9fe6ef7cfa9.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/about":["static/chunks/pages/about-f0fa81923039f0306c89.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
